2e48ef990089ed110c5fff8e89a93343cd277d77
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-06-10 13:41:07 +0200

    todo vue 3 composition api

---
+++ src/views/TodosView.vue
@@ -0,0 +1,107 @@
+<script setup>
+import { ref, watch } from "vue";
+import { uid } from "uid";
+import TodoCreator from "../components/TodoCreator.vue"
+import TodoItem from '../components/TodoItem.vue';  
+import { Icon } from "@iconify/vue";
+
+const todoList = ref([]);
+
+watch(todoList, () => {
+  setTodoListInLocalStorage();
+},{
+  deep: true,
+})
+
+const getTodoListFromLocalStorage = () => {
+  const savedTodos = JSON.parse(localStorage.getItem('todos'));
+  if (savedTodos)
+    todoList.value = savedTodos;
+}
+
+getTodoListFromLocalStorage();
+
+const setTodoListInLocalStorage = () => {
+  localStorage.setItem('todos',JSON.stringify(todoList.value));
+}
+
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null, 
+  });
+  
+}
+
+const toggleTodoCompleted = (todoIndex) => {
+  todoList.value[todoIndex].isCompleted = !todoList.value[todoIndex].isCompleted;
+}
+
+const toggleEditTodo = (todoIndex) => {
+  todoList.value[todoIndex].isEditing = !todoList.value[todoIndex].isEditing;
+}
+
+const updateTodoValue = (newValue,todoIndex) => {
+  todoList.value[todoIndex].todo = newValue;
+}
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+}
+
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator @create-todo="createTodo"/>
+    <ul class="todo-list" v-if="todoList.length > 0"> 
+      <TodoItem 
+        v-for="(todo,index) in todoList" 
+        :key="todo.id" 
+        :todo="todo"
+        :index="index"
+        @toggle-completed="toggleTodoCompleted"
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodoValue"
+        @delete-todo="deleteTodo"/>
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+</style>
